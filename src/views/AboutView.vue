dca3fe32f162dcd4848f6737eaff1ce962a0a3ca
uco <9417880+escapism@users.noreply.github.com>
2026-02-05 20:09:44 +0900

    add QR code, update README

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,5 +8,7 @@
       <p>使用前に必ず<router-link to="/about/notes">「ご利用上の注意」</router-link>をご確認ください。</p>
     </section>
     <p>ご意見・ご要望等あれば、作者<a href="https://x.com/escapist_uco" target="_blank">X (Twitter)</a>か<a href="https://marshmallow-qa.com/escapist_uco" target="_blank">マシュマロ</a>までお願いします。</p>
+      <p>実際に使ってみて良いと思ったら、是非お隣の人にもすすめてください。以下のQRコードからアクセスできます。</p>
+      <div class="qr"><img src="@/assets/qr.svg" alt="" title="QRコード"></div>
   </div>
 </template>
