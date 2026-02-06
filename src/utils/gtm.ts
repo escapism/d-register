f9368f72530d31a30f88a545b1a09c28d77e2468
uco <9417880+escapism@users.noreply.github.com>
2026-02-06 20:28:41 +0900

    update GTM

--- src/utils/gtm.ts
+++ src/utils/gtm.ts
@@ -1,11 +1,11 @@
 import { useGtm } from '@gtm-support/vue-gtm';
-const gtm = useGtm();
 
 export function gtmTrackEvent(event : string): void {
-  console.log(gtm, { event })
+  const gtm = useGtm();
   if (gtm) gtm.trackEvent({ event });
 }
 
 export function gtmTrackError(event : string): void {
+  const gtm = useGtm();
   if (gtm) gtm.trackEvent({ event, category: "error" });
 }
