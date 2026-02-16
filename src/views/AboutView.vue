98e5f3faa4be564143b15fe2e06a9a26b63d59ab
uco <9417880+escapism@users.noreply.github.com>
2026-02-16 20:07:43 +0900

    update AboutNotesView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ const router = useRoute()
       一度読み込んでおけば、オフラインでも動くため、電波の悪い会場でも使用できます。</p>
     <p class="note">大量の頒布物情報を編集したいときは、専用ツール「<a href="https://escapism.github.io/d-regi-editor/" target="_blank" rel="noopener">Dレジエディター</a>」をご利用ください（パソコン・タブレット推奨）。</p>
     <ImportantNotice />
-    <p>ご意見・ご要望等あれば、作者<a href="https://x.com/escapist_uco" target="_blank" rel="noopener">X (Twitter)</a>か<a href="https://marshmallow-qa.com/escapist_uco" target="_blank" rel="noopener">マシュマロ</a>までお願いします。</p>
+    <p>ご意見・ご要望等あれば、作者<a href="https://x.com/escapist_uco" target="_blank" rel="noopener noreferrer">X (Twitter)</a>か<a href="https://marshmallow-qa.com/escapist_uco" target="_blank" rel="noopener noreferrer">マシュマロ</a>までお願いします。</p>
       <p>実際に使ってみて良いと思ったら、是非お隣の人にもすすめてください。以下のQRコードからアクセスできます。</p>
       <div class="qr"><img src="@/assets/qr.svg" alt="" title="QRコード"></div>
   </div>
